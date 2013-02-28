691f73ba7dee7e5b6026905ae58b8eeb6e4d6d7c
Frank Raterink <frank@q42.nl>
2013-02-28 14:59:37 +0100

    first

---
+++ Font scaling/script.js
@@ -0,0 +1,19 @@
+﻿/*
+
+In de css geef je aan hoe groot te line-height moet zijn.
+Dat wordt dan gezien als de max-line-height. De min-line
+height die hierboven wordt aangegeven wordt gebruikt om
+naartoe te schalen wanneer de browserwindow kleiner wordt
+gemaakt. Dit gebeurt fluid.
+*/
+
+$(document).ready(function(){
+  $(".normal").minLineHeight(1.6);
+  $(".larger").minLineHeight(1.3);
+});
+
+
+
+/* class="hyphenate" op element waarop hyphens op moeten worden toegepast. */
+
+Hyphenator.run();
